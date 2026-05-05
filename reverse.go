5b7ab69da3863c93488ee0ce82db636a62c5a5ca
Shabinn-k <shhabin.07@gmail.com>
2026-05-05 11:01:33 +0530

    new problem

---
+++ reverse.go
@@ -0,0 +1,6 @@
+package main
+
+var rev =[]int{7,6,5,4,3,2,1}
+func Reverse(){
+
+}
---
+++ twoSum.go
@@ -0,0 +1,13 @@
+package main
+
+var nums=[]int{2,7,11,15}
+var target = 9
+var output = []int{0,1}
+
+// func TwoSum(nums []int,target int)[]int{
+// 	m:=make(map[int]int)
+// 	for i,num:=range nums{
+		
+// 	}
+	
+// }
