cb5ddaa8f6928e8c70e013ca656d5b6ae442407d
Shabinn-k <shhabin.07@gmail.com>
2026-05-12 12:33:08 +0530

    practising task using race cindition linkedlist

--- linkedList.go
+++ linkedList.go
@@ -70,6 +70,104 @@ import "fmt"
 
 // }
 
+// type Node struct{
+//     data int
+//     next *Node
+// }
+
+// func LL()*Node{
+//     node1:=&Node{data:10}
+//     node2:=&Node{data:10}
+//     node3:=&Node{data:30}
+//     node4:=&Node{data:40}
+//     node5:=&Node{data:30}
+    
+//     node1.next=node2
+//     node2.next=node3
+//     node3.next=node4
+//     node4.next=node5
+    
+//     head:=node1
+//     return head
+    
+// }
+
+// func Middle(head *Node){
+//     slow:=head
+//     fast:=head
+//     for fast!=nil&&fast.next!=nil{
+//         slow=slow.next
+//         fast=fast.next.next
+//     }
+//     fmt.Println("Middle :",slow.data)
+// }
+
+// func Print(head *Node){
+//     temp:=head
+//     for temp!=nil{
+//         fmt.Println(temp.data)
+//         temp=temp.next
+//     }
+// }
+
+// func Reverse(head *Node)*Node{
+//     var prev *Node
+//     current:=head
+//     for current!=nil{
+//         next:=current.next
+//         current.next=prev
+//         prev=current
+//         current=next
+//     }
+//     return prev
+// }
+
+// func Duplicate(head *Node)*Node{
+//     current:=head
+//     for current!=nil&&current.next!=nil{
+//         if current.data==current.next.data{
+//             current.next=current.next.next
+//         }else{
+//             current=current.next
+//         }
+//     }
+//     return head
+// }
+
+// func Duplicate(head *Node)*Node{
+//     var prev *Node
+//     current:=head
+//     seen:=make(map[int]bool)
+    
+//     for current!=nil{
+//         if seen[current.data]{
+//             prev.next=current.next
+//         }else{
+//             seen[current.data]=true
+//             prev=current
+//         }
+//         current=current.next
+//     }
+//     return head
+// }
+
+// func main(){
+//     head:=LL()
+    
+//     fmt.Println("Original :")
+//     Print(head)
+    
+//     Middle(head)
+    
+//     fmt.Println("Reverse :")
+//     tail:=Reverse(head)
+//     Print(tail)
+    
+//     fmt.Println("Duplicate :")
+//     dupe:=Duplicate(LL())
+//     Print(dupe)
+// }
+
 type Node struct{
 	data int
 	Next *Node
