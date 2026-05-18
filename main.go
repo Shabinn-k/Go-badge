682fa47a44e6a91c91050808ad9483a92370c13c
Shabinn-k <shhabin.07@gmail.com>
2026-05-18 18:18:56 +0530

    some problems

--- main.go
+++ main.go
@@ -2,279 +2,234 @@ package main
 
 import "fmt"
 
-// "fmt"
-
-// func main() {
-// 1
-// result1 := TwoSum(nums, target)
-// fmt.Println(result1)
-
-// 2
-// result2 := Reverse(rev)
-// fmt.Println(result2)
-
-// 3
-// result3,result4:=Max(maxNums)
-// fmt.Println(result3,result4)
-
-// 4
-// result5:=Sum(Array1)
-// fmt.Println(result5)
-
-// 5
-// Mix()
-
-// 6
-// head:=Linked()
-// Print(head)
-// Middle(head)
-// reverseHead:=ReverseLL(head)
-// Print(reverseHead)
-
-// 7
-// array:=[]int{1,2,3,4,5,2,3}
-// value:=2
-// for i,v:=range array{
-// 	if v==value{
-// 		array = append(array[:i],array[i+1:]...)
-// 	break
-// 	}
-// }
-// array = append(array[:ind],array[ind+1:]...)
-// 	dupe:=make(map[int]bool)
-// 	res:=[]int{}
-// 	for _,v:=range array{
-// 		if !dupe[v]{
-// 			dupe[v]=true
-// 			res = append(res, v)
-// 		}
-// 	}
-// 	fmt.Println(res)
-
-// 8
-// Stack()
-// Queue()
-
-// 9
-// DoubleLL()
-
-// 10
-// 	Race()
-// }
-
-// var str = "madamk"
-
-// func main() {
-// 	l := 0
-// 	r := len(str) - 1
-// 	g := []rune(str)
-// 	for l < r {
-// 		if g[r] != g[l] {
-// 			fmt.Println(false)
-// 			return
-// 		}
-// 		l++
-// 		r--
-// 	}
-// 	fmt.Println(true)
-// }
-
+// Print numbers from 1–100
 // func main(){
-// 	arr:=[]int{1,2,3,4,0,5}
-// 	max:=arr[0]
-// 	min:=arr[0]
-
-// 	for i:=1;i<len(arr);i++{
-// 		if arr[i]>max{
-// 			max=arr[i]
-// 		}else{
-// 			min=arr[i]
-// 		}
+// 	for i:=1;i<=100;i++{
+// 		fmt.Println(i)
+		
 // 	}
-// 	fmt.Println("max",max)
-// 	fmt.Println("Min",min)
 // }
 
+// Print even numbers from 1–100
 // func main(){
-// 	arr := []int{10, 5, 8, 20, 15}
-// 	large:=arr[0]
-// 	second:=arr[0]
+// 	for i:=1;i<=100;i+=2{
+// 		fmt.Println(i)
+// 	}
+// }
 
-// 	for i:=1;i<len(arr);i++{
-// 		if arr[i]>large{
+// Reverse a string
+// func main(){
+// str:="Hello World"
+// r:=[]rune(str)
+// 	f:=0
+// 	l:=len(r)-1
+// 	for f<l{
+// 		r[f],r[l]=r[l],r[f]
+// 		f++
+// 		l--
+// 	}
+// 	fmt.Println(string(r))
+// }
+
+// Check palindrome string
+// func main(){
+// 	str:="madam"
+// 	p:=true
+// 	f:=0
+// 	l:=len(str)-1
+// 	for f<l{
+// 		if str[f]!=str[l]{
+// 			p=false
+// 		}
+// 		f++
+// 		l--
+// 	}
+// 	fmt.Println(p)
+// }
+
+// Find factorial using recursion
+// func Recursion(n int)int{
+// 	if n<=1{
+// 		return 1
+// 	}
+// 	return n*Recursion(n-1)
+// }
+// func main(){
+// 	answer:=Recursion(10)
+// 	fmt.Println(answer)
+// }
+
+// Find factorial using loop
+// func main(){
+// 	n:=10
+// 	fact:=1
+// 	if n<=1{
+// 		fmt.Println(1)
+// 		return
+// 	}
+// 	for i:=1;i<=n;i++{
+// 		fact=fact*i
+// 	}
+// 	fmt.Println(fact)
+// }
+
+// Swap two numbers without third variable
+// func main(){
+// 	a:=432
+// 	b:=6423
+// 	fmt.Println("Before Swap")
+// 	fmt.Println(a)
+// 	fmt.Println(b)
+
+// 	a=a+b
+// 	b=a-b
+// 	a=a-b
+// 	fmt.Println("After Swap")
+// 	fmt.Println(a)
+// 	fmt.Println(b)
+// }
+
+// Find largest element in array
+// func main(){
+// 	array:=[]int{1,10,2,3,4,5}
+// 	large:=array[0]
+// 	for i:=1;i<len(array);i++{
+// 		if array[i]>large{
+// 			large=array[i]
+// 		}
+// 	}
+// 	fmt.Println(large)
+// }
+
+// Find second largest element
+// func main(){
+// 	array:=[]int{5,2,10,3,9,4,1}
+// 	large:=array[0]
+// 	second:=array[0]
+
+// 	for i:=1;i<len(array);i++{
+// 		if array[i]>large{
 // 			second=large
-// 			large=arr[i]
-// 		}else if arr[i]>second&&arr[i]!=large{
-// 			second=arr[i]
+// 			large=array[i]
+// 		}else if array[i]>second&&array[i]!=large{
+// 			second=array[i]
 // 		}
 // 	}
 // 	fmt.Println(second)
 // }
 
-// var arr=[]int{1,2,3,4,5}
-// var targets=9
-
+// Remove duplicates from slice
 // func main(){
-
-// seen:=make(map[int]int)
-// 	for i,n:=range arr{
-// 		get:=targets-n
-// 		if ind,found:=seen[get];found{
-// 			fmt.Println(ind,i)
-// 			return
+// 	array:=[]int{1,2,3,4,1,2,5,6}
+// 	dupe:=[]int{}
+// 	m:=make(map[int]bool)
+// 	for _,v:=range array{
+// 		if !m[v]{
+// 			m[v]=true
+// 			dupe=append(dupe, v)
 // 		}
-// 		seen[n]=i
 // 	}
-// 	fmt.Println("nothing")
+// 	fmt.Println(dupe)
 // }
 
+// Reverse an array
 // func main(){
-// arr:=[]int{1,2,3,4,5}
-// l:=0
-// f:=len(arr)-1
-// for l<f{
-// 	arr[l],arr[f]=arr[f],arr[l]
-// 	l++
-// 	f--
+// array:=[]int{1,2,3,4,5}
+// f:=0
+// l:=len(array)-1
+// for f<l{
+// 	array[f],array[l]=array[l],array[f]
+// 	f++
+// 	l--
 // }
-// fmt.Println(arr)
-// }
-
-// type Shape interface{
-// 	Circle() int
-// 	Rectangle() int
+// fmt.Println(array)
 // }
 
-// type Area struct{
-// 	Length int
-// 	Width int
-// }
-
-// func (a Area)Circle()int{
-// 	return 2*3*a.Length
-// }
-// func (a Area)Rectangle()int{
-// 	return a.Length*a.Width
-// }
-
-// func Trial(s Shape){
-// 	fmt.Println(s.Circle())
-// 	fmt.Println(s.Rectangle())
-// }
-
-// func  main(){
-// 	aa:=Area{
-// 		Width: 90,
-// 		Length: 10,
-// 	}
-// 	Trial(aa)
-// }
-
+// Count vowels in string
 // func main(){
-// 	str:="Shabin is a good boy"
-// 	m:=make(map[rune]int)
-// 	for _,v:=range str{
-// 		m[v]++
-// 	}
-// 	for a,b:=range m{
-// 		fmt.Printf("%c: %d\n",a,b)
-// 	}
-// }
-
-// func main(){
-// 	str:="Shabin is a good boy"
-// 	m:=make(map[int]string)
+// 	str:="hyy my name is shabin"
+// 	v:=0
 // 	for i:=0;i<len(str);i++{
-// 		m[i]=string(str[i])
+// 		ch:=str[i]
+// 		if ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'{
+// 			v++
+// 		}
 // 	}
-// 	fmt.Println(m)
+// 	fmt.Println(v)
 // }
 
+// Count words in sentence
 // func main(){
-// 	str:="Shabin is a good boy "
-// 	a:=strings.Fields(str)
-// 	m:=make(map[string]int)
+// 	words:="Hyy my name is shabin"
+// 	count:=0
+// 	inWord:=false
+
+// 	for i:=0;i<len(words);i++{
+// 		if words[i]!=' '&&!inWord{
+// 			inWord=true
+// 			count++
+// 		}
+// 		if words[i]==' '{
+// 			inWord=false
+// 		}
+// 	}
+// 	fmt.Println(count)
+// }
+
+//Check anagram strings
+// func main(){
+// 	a:="listen"
+// 	b:="silent"
+// 	if len(a)!=len(b){
+// 		fmt.Println(false)
+// 		return
+// 	}
+// 	m:=make(map[rune]int)
 // 	for _,v:=range a{
 // 		m[v]++
 // 	}
-// 	fmt.Println(m)
+// 	for _,v:=range b{
+// 		m[v]--
+// 	}
+// 	for _,v:=range m{
+// 		if v!=0{
+// 			fmt.Println(false)
+// 			return
+// 		}
+// 	}
+// 	fmt.Println(true)
 // }
 
-// var mu sync.Mutex
-// var bank int
-
-// func Deposits(wg *sync.WaitGroup){
-// 	defer wg.Done()
-// 	mu.Lock()
-// 	bank+=10
-// 	mu.Unlock()
+// Fibonacci series
+// func main(){
+// 	a:=0
+// 	b:=1
+// 	for i:=0;i<10;i++{
+// 		fmt.Println(a)
+// 		next:=a+b
+// 		a=b
+// 		b=next
+// 	}
 // }
 
 // func main(){
-// 	var wg sync.WaitGroup
-// 	for i:=0;i<10000;i++{
-// 		wg.Add(1)
-// 		go Deposits(&wg)
-// 	}
-// 	wg.Wait()
-// 	fmt.Println(bank)
-// }
-
-// func main(){
-// target:=9
-// arr:=[]int{2,4,5,6,8}
-// m:=make(map[int]int)
-
-// for i,v:=range arr{
-// 	get:=target-v
-// 	if ind,fou:=m[get];fou{
-// 		fmt.Println(ind,i)
-// 		return
-// 	}
-// 	m[v]=i
-// }
-// fmt.Println("Nothing")
-// }
-
-// func Odds(oddch chan int){
-// 	for i:=1;i<10;i+=2{
-// 		oddch <-i
-// 	}
-// close(oddch)
-// }
-// func Evens(evench chan int){
-// for i:=0;i<10;i+=2{
-// 	evench<-i
-// }
-// 	close(evench)
-// }
-
-// func main(){
-// 	oddch:=make(chan int)
-// 	evench:=make(chan int)
-
-// 	go Odds(oddch)
-// 	go Evens(evench)
-
-// 	for n:=range evench{
-// 		fmt.Println(n)
-// 	}
-// 	for n:=range oddch{
-// 		fmt.Println(n)
+// 	num:=70
+// isPrime:=true
+// 	if num<=1{
+// 		isPrime=false
+// 	}else{
+// 	for i:=2;i<num;i++{
+// 		if num%i==0{
+// 			isPrime=false
+// 			break
+// 		}
 // 	}
 // }
-func main(){
-str:="madam"
-f:=0
-p:=false
-l:=len(str)-1
-for f>l{
-	if str[f]!=str[l]{
-		p=true
-		break
-	}
-	f++
-	l--
-}
-	fmt.Println(p)
-}
+// if isPrime{
+// 	fmt.Println("Is prime")
+// }else{
+// 	fmt.Println("Not prime")
+// }
+// }
+
+// Sum of digits of a number
--- trail.go
+++ trail.go
@@ -70,3 +70,278 @@ package main
 //     }
 //     fmt.Println(dupe)
 // }
+
+// func main() {
+// 1
+// result1 := TwoSum(nums, target)
+// fmt.Println(result1)
+
+// 2
+// result2 := Reverse(rev)
+// fmt.Println(result2)
+
+// 3
+// result3,result4:=Max(maxNums)
+// fmt.Println(result3,result4)
+
+// 4
+// result5:=Sum(Array1)
+// fmt.Println(result5)
+
+// 5
+// Mix()
+
+// 6
+// head:=Linked()
+// Print(head)
+// Middle(head)
+// reverseHead:=ReverseLL(head)
+// Print(reverseHead)
+
+// 7
+// array:=[]int{1,2,3,4,5,2,3}
+// value:=2
+// for i,v:=range array{
+// 	if v==value{
+// 		array = append(array[:i],array[i+1:]...)
+// 	break
+// 	}
+// }
+// array = append(array[:ind],array[ind+1:]...)
+// 	dupe:=make(map[int]bool)
+// 	res:=[]int{}
+// 	for _,v:=range array{
+// 		if !dupe[v]{
+// 			dupe[v]=true
+// 			res = append(res, v)
+// 		}
+// 	}
+// 	fmt.Println(res)
+
+// 8
+// Stack()
+// Queue()
+
+// 9
+// DoubleLL()
+
+// 10
+// 	Race()
+// }
+
+// var str = "madamk"
+
+// func main() {
+// 	l := 0
+// 	r := len(str) - 1
+// 	g := []rune(str)
+// 	for l < r {
+// 		if g[r] != g[l] {
+// 			fmt.Println(false)
+// 			return
+// 		}
+// 		l++
+// 		r--
+// 	}
+// 	fmt.Println(true)
+// }
+
+// func main(){
+// 	arr:=[]int{1,2,3,4,0,5}
+// 	max:=arr[0]
+// 	min:=arr[0]
+
+// 	for i:=1;i<len(arr);i++{
+// 		if arr[i]>max{
+// 			max=arr[i]
+// 		}else{
+// 			min=arr[i]
+// 		}
+// 	}
+// 	fmt.Println("max",max)
+// 	fmt.Println("Min",min)
+// }
+
+// func main(){
+// 	arr := []int{10, 5, 8, 20, 15}
+// 	large:=arr[0]
+// 	second:=arr[0]
+
+// 	for i:=1;i<len(arr);i++{
+// 		if arr[i]>large{
+// 			second=large
+// 			large=arr[i]
+// 		}else if arr[i]>second&&arr[i]!=large{
+// 			second=arr[i]
+// 		}
+// 	}
+// 	fmt.Println(second)
+// }
+
+// var arr=[]int{1,2,3,4,5}
+// var targets=9
+
+// func main(){
+
+// seen:=make(map[int]int)
+// 	for i,n:=range arr{
+// 		get:=targets-n
+// 		if ind,found:=seen[get];found{
+// 			fmt.Println(ind,i)
+// 			return
+// 		}
+// 		seen[n]=i
+// 	}
+// 	fmt.Println("nothing")
+// }
+
+// func main(){
+// arr:=[]int{1,2,3,4,5}
+// l:=0
+// f:=len(arr)-1
+// for l<f{
+// 	arr[l],arr[f]=arr[f],arr[l]
+// 	l++
+// 	f--
+// }
+// fmt.Println(arr)
+// }
+
+// type Shape interface{
+// 	Circle() int
+// 	Rectangle() int
+// }
+
+// type Area struct{
+// 	Length int
+// 	Width int
+// }
+
+// func (a Area)Circle()int{
+// 	return 2*3*a.Length
+// }
+// func (a Area)Rectangle()int{
+// 	return a.Length*a.Width
+// }
+
+// func Trial(s Shape){
+// 	fmt.Println(s.Circle())
+// 	fmt.Println(s.Rectangle())
+// }
+
+// func  main(){
+// 	aa:=Area{
+// 		Width: 90,
+// 		Length: 10,
+// 	}
+// 	Trial(aa)
+// }
+
+// func main(){
+// 	str:="Shabin is a good boy"
+// 	m:=make(map[rune]int)
+// 	for _,v:=range str{
+// 		m[v]++
+// 	}
+// 	for a,b:=range m{
+// 		fmt.Printf("%c: %d\n",a,b)
+// 	}
+// }
+
+// func main(){
+// 	str:="Shabin is a good boy"
+// 	m:=make(map[int]string)
+// 	for i:=0;i<len(str);i++{
+// 		m[i]=string(str[i])
+// 	}
+// 	fmt.Println(m)
+// }
+
+// func main(){
+// 	str:="Shabin is a good boy "
+// 	a:=strings.Fields(str)
+// 	m:=make(map[string]int)
+// 	for _,v:=range a{
+// 		m[v]++
+// 	}
+// 	fmt.Println(m)
+// }
+
+// var mu sync.Mutex
+// var bank int
+
+// func Deposits(wg *sync.WaitGroup){
+// 	defer wg.Done()
+// 	mu.Lock()
+// 	bank+=10
+// 	mu.Unlock()
+// }
+
+// func main(){
+// 	var wg sync.WaitGroup
+// 	for i:=0;i<10000;i++{
+// 		wg.Add(1)
+// 		go Deposits(&wg)
+// 	}
+// 	wg.Wait()
+// 	fmt.Println(bank)
+// }
+
+// func main(){
+// target:=9
+// arr:=[]int{2,4,5,6,8}
+// m:=make(map[int]int)
+
+// for i,v:=range arr{
+// 	get:=target-v
+// 	if ind,fou:=m[get];fou{
+// 		fmt.Println(ind,i)
+// 		return
+// 	}
+// 	m[v]=i
+// }
+// fmt.Println("Nothing")
+// }
+
+// func Odds(oddch chan int){
+// 	for i:=1;i<10;i+=2{
+// 		oddch <-i
+// 	}
+// close(oddch)
+// }
+// func Evens(evench chan int){
+// for i:=0;i<10;i+=2{
+// 	evench<-i
+// }
+// 	close(evench)
+// }
+
+// func main(){
+// 	oddch:=make(chan int)
+// 	evench:=make(chan int)
+
+// 	go Odds(oddch)
+// 	go Evens(evench)
+
+// 	for n:=range evench{
+// 		fmt.Println(n)
+// 	}
+// 	for n:=range oddch{
+// 		fmt.Println(n)
+// 	}
+// }
+// func main(){
+// str:="madam"
+// f:=0
+// p:=false
+// l:=len(str)-1
+// for f>l{
+// 	if str[f]!=str[l]{
+// 		p=true
+// 		break
+// 	}
+// 	f++
+// 	l--
+// }
+// 	fmt.Println(p)
+// }
